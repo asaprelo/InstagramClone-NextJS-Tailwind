c5c971e4feeac0d863acac17c096c25335a1cea7
lorenzo <l.ceglia@fabless.it>
2022-06-20 13:37:27 +0200

    Header Padding Fix

--- components/Header.js
+++ components/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
     }
 
     return (
-        <header className="border-b-2 w-full bg-white sticky z-50 top-0">
+        <header className="border-b-2 w-full xs:pl-2 xs:pr-2 bg-white sticky z-50 top-0">
             <div className="flex p-1 md:max-w-4xl m-auto justify-between">
 
                 <div className="flex items-center">
