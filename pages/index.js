70099ff4ec06ff78a56dda530bbf1089a0f98da9
lorenzo <l.ceglia@fabless.it>
2022-06-20 15:07:38 +0200

    end demo?

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="bg-ig_bg min-w-max">
           <Head>
               <title>Lorenzgram</title>
-              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+              <meta name="viewport" content="width=1000"/>
           </Head>
         <Header/>
         <Container/>
