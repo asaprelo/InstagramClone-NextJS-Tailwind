148f95645671334d79483c27346004f05cc8af5f
lorenzo <l.ceglia@fabless.it>
2022-06-20 14:19:31 +0200

    fake posts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['instagram.fcia7-2.fna.fbcdn.net','upload.wikimedia.org','play-lh.googleusercontent.com','static.fanpage.it','melmagazine.com']
+    domains: ['instagram.fcia7-2.fna.fbcdn.net','upload.wikimedia.org','play-lh.googleusercontent.com','static.fanpage.it','melmagazine.com','i.ibb.co']
   }
 }
